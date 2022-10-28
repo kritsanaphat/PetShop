6d9e1960a569cf199a6f714bf3619680ecb3ff75
Kritsanaphat <63010040@kmitl.ac.th>
2022-10-28 14:44:35 +0700

    try to generate uuid

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ func main() {
 	DB := db.Init()
 	h := handlers.New(DB)
 	router := mux.NewRouter()
-
+	// p := models.User{Fullname: "krit"}
+	// fmt.Print(p)
 	router.HandleFunc("/user", h.AddUser).Methods(http.MethodPost)
 	http.ListenAndServe(":8080", router)
 }
