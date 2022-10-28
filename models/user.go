857687ed09b529633462bd341109c21ae9d16507
Kritsanaphat <63010040@kmitl.ac.th>
2022-10-28 14:10:37 +0700

    try to generate uuid

--- models/user.go
+++ models/user.go
@@ -2,11 +2,21 @@ package models
 
 import (
 	"github.com/gofrs/uuid"
+	"github.com/jinzhu/gorm"
 )
 
 type User struct {
-	ID       uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()"`
+	ID       uuid.UUID `gorm:"type:uuid;primary_key;"`
 	Fullname string    `json:"fullname"`
 	Password string    `json:"password"`
 	Email    string    `json:"email"`
+}
+
+// BeforeCreate will set a UUID rather than numeric ID.
+func (base *User) BeforeCreate(scope *gorm.Scope) error {
+	uuid, err := uuid.NewV4()
+	if err != nil {
+		return err
+	}
+	return scope.SetColumn("ID", uuid)
 }
