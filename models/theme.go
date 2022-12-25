9fe7592331215a2477f71d216b0445746f1c7133
Kritsanaphat <63010040@kmitl.ac.th>
2022-12-26 02:17:00 +0700

    add theme model

---
+++ models/theme.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/gofrs/uuid"
+	"gorm.io/gorm"
+)
+
+type Theme struct {
+	gorm.Model
+	ThemeID  uuid.UUID `json:"themeID" gorm:"primary_key;"`
+	AuthorID uuid.UUID `json:"authorID"`
+	Topic    string    `json:"topic"`
+	Content  string    `json:"content"`
+}
+
+type Comment struct {
+	gorm.Model
+	ThemeID   uuid.UUID `json:"themeID" gorm:"foreign_key;"`
+	CommentID uuid.UUID `json:"commentID" gorm:"primary_key;"`
+	Comment   string    `json:"comment"`
+}
